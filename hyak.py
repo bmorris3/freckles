0b10b796a315b267ea6fb15f63fa212250c8c0dd
Brett Morris <brettmorris21@gmail.com>
2017-08-11 17:40:56 -0700

    switch to 8 core

--- hyak.py
+++ hyak.py
@@ -31,7 +31,7 @@ submit_template = """#!/bin/bash
 #PBS -q bf
 
 ## NUMBER nodes, CPUs per node, and MEMORY
-#PBS -l nodes=1:ppn=16,mem=20gb,feature=intel,feature=16core
+#PBS -l nodes=1:ppn=8,mem=20gb,feature=intel,feature=8core
 
 ## WALLTIME (defaults to 1 hour as the minimum, specify > 1 hour longer jobs)
 #PBS -l walltime={walltime}
